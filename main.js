dc7e82c9ded089dba1d848261fe844999215875e
HippErger <berger.clayton@gmail.com>
2020-01-20 14:27:46 -0600

    added in initial boilerplate for Towers

--- main.js
+++ main.js
@@ -1,71 +1,108 @@
 'use strict';
 
-// brings in the assert module for unit testing
 const assert = require('assert');
-// brings in the readline module to access the command line
 const readline = require('readline');
-// use the readline module to print out to the command line
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// An object that represents the three stacks of Towers of Hanoi; 
+  // * each key is an array of Numbers: 
+    // * A is the far-left, 
+    // * B is the middle, 
+    // * C is the far-right stack
+      // * Each number represents the largest to smallest tokens: 
+        // * 4 is the largest, 
+        // * 1 is the smallest
 
-const pigLatin = (word) => {
+let stacks = {
+  a: [4, 3, 2, 1],
+  b: [],
+  c: []
+};
 
+// Start here. What is this function doing?
+const printStacks = () => {
+  console.log("a: " + stacks.a);
+  console.log("b: " + stacks.b);
+  console.log("c: " + stacks.c);
+}
+
+// Next, what do you think this function should do?
+const movePiece = () => {
   // Your code here
 
 }
 
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
+// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
+const isLegal = () => {
+  // Your code here
+
+}
+
+// What is a win in Towers of Hanoi? When should this function run?
+const checkForWin = () => {
+  // Your code here
+
+}
+
+// When is this function called? What should it do with its argument?
+const towersOfHanoi = (startStack, endStack) => {
+  // Your code here
+
+}
+
 const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
+  printStacks();
+  rl.question('start stack: ', (startStack) => {
+    rl.question('end stack: ', (endStack) => {
+      towersOfHanoi(startStack, endStack);
+      getPrompt();
+    });
   });
 }
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
+// Tests
+
 if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+  describe('#towersOfHanoi()', () => {
+    it('should be able to move a block', () => {
+      towersOfHanoi('a', 'b');
+      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
     });
   });
+
+  describe('#isLegal()', () => {
+    it('should not allow an illegal move', () => {
+      stacks = {
+        a: [4, 3, 2],
+        b: [1],
+        c: []
+      };
+      assert.equal(isLegal('a', 'b'), false);
+    });
+    it('should allow a legal move', () => {
+      stacks = {
+        a: [4, 3, 2, 1],
+        b: [],
+        c: []
+      };
+      assert.equal(isLegal('a', 'c'), true);
+    });
+  });
+  describe('#checkForWin()', () => {
+    it('should detect a win', () => {
+      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      assert.equal(checkForWin(), true);
+      stacks = { a: [1], b: [4, 3, 2], c: [] };
+      assert.equal(checkForWin(), false);
+    });
+  });
+
 } else {
 
   getPrompt();
 
 }
-
-
-
-
-
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
