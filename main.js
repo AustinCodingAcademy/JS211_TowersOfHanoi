551e249a1c4ea001b1aa24c2b555ddccb7cf377b
THam53 <texas01988@gmail.com>
2020-05-24 21:50:38 -0500

    new

--- main.js
+++ main.js
@@ -1,108 +1,65 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var towerSelected = false;
+var selectedTowerId;
+var $selectedPiece;
+var countMoves = 0;
 
-// An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+$(document).ready(init);
 
-let stacks = {
-  a: [4, 3, 2, 1],
-  b: [],
-  c: []
-};
-
-// Start here. What is this function doing?
-const printStacks = () => {
-  console.log("a: " + stacks.a);
-  console.log("b: " + stacks.b);
-  console.log("c: " + stacks.c);
+function init(){
+    var $towers = $('.towerBase');
+    $towers.on('click', towerClicked);
 }
 
-// Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
+function towerClicked(event){
+    var currentTowerId = this.id;
+    //unselect the piece
+    if (towerSelected){
+        if (this.id === selectedTowerId){
+        towerClicked = false;
+        $selectedPiece.removeClass('selected');
 
+      } else{
+        var topPieceId = $selectedPiece.attr('id');
+        var bottomPieceId = $(this).children(":first").attr('id');
+
+        // piece gets moved to new tower
+        if (!bottomPieceId || validMove(topPieceId, bottomPieceId)){
+            $selectedPiece.remove();
+            $(this).prepend($selectedPiece);
+            towerSelected = false;
+            $selectedPiece.removeClass('selected');
+            countMoves += 1;
+            $('#displayMoves').text("Moves: " + countMoves);
+
+        // invalid move
+        } else {
+            towerSelected = false;
+            $selectedPiece.removeClass('selected');
+        }
+      }
+// first tower gets selected
+    } else {
+      $selectedPiece = $(this).children(":first");
+      $selectedPiece.addClass("selected");
+      towerSelected = true;
+      selectedTowerId = this.id;
+  }
+  checkGameWon();
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
-
+function validMove(topPieceId, bottomPieceId){
+    return parseInt(topPieceId[5]) < parseInt(bottomPieceId[5])
 }
 
-// What is a win in Towers of Hanoi? When should this function run?
-const checkForWin = () => {
-  // Your code here
-
+function checkGameWon(){
+    var count = $("#tower3").children().length;
+    if (count === 4)
+        return youWin();
 }
 
-// When is this function called? What should it do with its argument?
-const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
-
-}
-
-const getPrompt = () => {
-  printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
-    });
-  });
-}
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#towersOfHanoi()', () => {
-    it('should be able to move a block', () => {
-      towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
-    });
-  });
-
-  describe('#isLegal()', () => {
-    it('should not allow an illegal move', () => {
-      stacks = {
-        a: [4, 3, 2],
-        b: [1],
-        c: []
-      };
-      assert.equal(isLegal('a', 'b'), false);
-    });
-    it('should allow a legal move', () => {
-      stacks = {
-        a: [4, 3, 2, 1],
-        b: [],
-        c: []
-      };
-      assert.equal(isLegal('a', 'c'), true);
-    });
-  });
-  describe('#checkForWin()', () => {
-    it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
-      assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
-      assert.equal(checkForWin(), false);
-    });
-  });
-
-} else {
-
-  getPrompt();
-
+function youWin(){
+    $('body').css("background-color", "LightSalmon");
+    $('#displayOutput').text("You win!");
 }
