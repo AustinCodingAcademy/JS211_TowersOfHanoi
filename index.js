c192c00c253fb8b8d1cec0e362ac9cb4a20b8e98
Stephen <stephen.lyssy@gmail.com>
2022-07-16 16:06:56 -0500

    Added comments to document my code.

--- index.js
+++ index.js
@@ -10,30 +10,30 @@
 let stone = null;
 let dropStoneFunction = false;
 let moves = 0;
-console.log(dropStoneFunction);
-// this function is called when a row is clicked.
-// Open your inspector tool to see what is being captured and can be used.
+// #This function selects the row and calls the toggle function. When a row is
+// #clicked, this function is run.
 const selectRow = (row) => {
   const currentRow = row.getAttribute('data-row');
 
   // console.log('Yay, we clicked an item', row);
   // console.log("Here is the stone's id: ", row.id);
   // console.log("Here is the stone's data-size: ", currentRow);
-
+  //# Calling toggle function to determine whether or not we will be picking up a stone or dropping a stone.
   toggleFunction(row.id, stone);
   console.log(dropStoneFunction);
 };
 
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
+// #Function to pick up the last stone. This is done by removing the last child
+// #from the row parent element.
 const pickUpStone = (rowID) => {
-  console.log(rowID);
+  // console.log(rowID);
   const selectedRow = document.getElementById(rowID);
-  console.log(selectedRow);
+  // console.log(selectedRow);
   stone = selectedRow.removeChild(selectedRow.lastElementChild);
-  console.log(stone);
+  // console.log(stone);
 };
 
+// # Function to drop the stone. This is done by selecting the row where the stone needs to be dropped and verifying if there is currently anything in that row. If so, we need to make sure that the existing stone is larger than the stone we want to replace.
 const dropStone = (rowID, stone) => {
   const movesCounter = document.querySelector('.move-counter');
   const selectedRow = document.getElementById(rowID);
@@ -44,14 +44,22 @@ const dropStone = (rowID, stone) => {
     existingStone === null ||
     existingStone.getAttribute('data-size') > stone.getAttribute('data-size')
   ) {
+    // #If the stone can be dropped we append the stone element and move the
+    // #counter up by one.
     selectedRow.appendChild(stone);
+    moves++;
   } else {
+    // #If not, we initiate an alert and keep the dropStoneFunction toggle
+    // #variable set to false so we can drop the stone in a valid row. We don't
+    // #want the dropStoneFunction to change because it will make the next move
+    // #a pick up stone function.
     window.alert('Invalid Move!');
     dropStoneFunction = false;
   }
 
   stone = null;
-  moves++;
+
+  // #Here we are deleting the counter and adding one each time a move is logged.
   movesCounter.innerHTML = '';
   movesCounter.insertAdjacentHTML(
     'afterbegin',
@@ -62,17 +70,19 @@ const dropStone = (rowID, stone) => {
   checkForWin(selectedRow);
 };
 
-// You could use this function to drop the stone but you'll need to toggle
-// between pickUpStone & dropStone
-
+// #This function toggles between the dropStone function and the pickUpStone
+// #function. This is done by watching the dropStoneFunction boolean value.
 const toggleFunction = (row, stone) => {
   dropStoneFunction ? dropStone(row, stone) : pickUpStone(row);
   dropStoneFunction = !dropStoneFunction;
 };
 
+// # We are checking for win by counting the number of child elements. Once child elements equal 4, the game is won.
 const checkForWin = (row) => {
   if (row.childElementCount === 4) {
     window.alert('You Win!');
   }
 };
+
+const playGame = () => {};
 // * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
